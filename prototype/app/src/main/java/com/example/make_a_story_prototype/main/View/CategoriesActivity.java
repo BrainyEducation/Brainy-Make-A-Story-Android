a3ae50515ddead6fd8348bacc980c741cf5c9fac
Ashley DelVentura <ashley@delventura.com>
2019-11-07 02:26:44 -0500

    fix to status bar

--- prototype/app/src/main/java/com/example/make_a_story_prototype/main/View/CategoriesActivity.java
+++ prototype/app/src/main/java/com/example/make_a_story_prototype/main/View/CategoriesActivity.java
@@ -1,7 +1,13 @@
 package com.example.make_a_story_prototype.main.View;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.example.make_a_story_prototype.R;
 
@@ -9,6 +15,7 @@ import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,8 +34,44 @@ public class CategoriesActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // workaround for status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+            getSupportActionBar().setTitle("Categories");
+        }
+
         Log.d(TAG, "onCreate: started");
         initImageBitmaps();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.icon_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(this, "Todo: back button", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.storybook:
+                Toast.makeText(this, "Todo: storybook button", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     private void initImageBitmaps() {
