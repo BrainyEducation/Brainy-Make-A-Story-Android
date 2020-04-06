c118b0dc5afef34912a6106ee937be9c68c870e7
cmei21 <35357352+cmei21@users.noreply.github.com>
2020-04-06 18:15:56 -0400

    added word and category entities

---
+++ prototype/app/src/main/java/com/example/make_a_story_prototype/main/data/Word/db/Category.java
@@ -0,0 +1,37 @@
+package com.example.make_a_story_prototype.main.data.Word.db;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "categories_table")
+public class Category {
+
+    @PrimaryKey
+    private int categoryId;
+    private String name;
+    private int imageResource;
+    private int audioResource;
+
+    public Category(int categoryId, String name, int imageResource, int audioResource) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.imageResource = imageResource;
+        this.audioResource = audioResource;
+    }
+
+    public int getId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public int getAudioResource() {
+        return audioResource;
+    }
+}
